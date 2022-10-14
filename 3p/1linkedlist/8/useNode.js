557e45086f83982f430c28e798718d441625a3d5
YAHIR ALEJANDRO MAGAÑA CARRILLO <61678912+yahir512@users.noreply.github.com>
2022-10-14 23:45:10 +0000

    nombre

---
+++ 3p/1linkedlist/8/useNode.js
@@ -0,0 +1,8 @@
+//
+//
+//
+import Node from '../../../yamc/linked/node.js'
+
+let n=new Node('b')
+
+console.log(n)
--- 3p/1linkedlist/8/useNode.js .js
+++
@@ -1,8 +0,0 @@
-//
-//
-//
-import Node from '../../../jrgp/linked/node.js'
-
-let n=new Node('b')
-
-console.log(n)
